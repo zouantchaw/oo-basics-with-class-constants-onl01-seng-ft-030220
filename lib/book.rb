b129257a90e9f68a7e034f1b5a4efa2c5910a4fc
Wielfried Zouantcha <zouantchaw74@gmail.com>
2020-03-16 02:48:21 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,6 +1,6 @@
 class Book
   attr_accessor :author, :page_count, :genre
-  attr_reader :title
+  attr_reader :title, :genre
   
   GENRES = []
 
@@ -11,5 +11,7 @@ class Book
   def turn_page
     puts "Flipping the page...wow, you read fast!"
   end
+  
+  
 
 end
