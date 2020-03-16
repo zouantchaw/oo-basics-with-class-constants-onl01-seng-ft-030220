b7ddeaaf8415b638fb72c1b17c5828e136edfe10
Wielfried Zouantcha <zouantchaw74@gmail.com>
2020-03-16 02:57:14 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -4,14 +4,17 @@ class Shoe
   
   BRANDS = []
 
-  def brand=(brand)
+  def initialize(brand)
     @brand = brand
-    BRANDS << brand
   end
 
   def cobble
     self.condition = "new"
     puts "Your shoe is as good as new!"
   end
+  
+  def brand=(brand)
+    @brand = brand 
+    BRANDS << brand
 
 end
